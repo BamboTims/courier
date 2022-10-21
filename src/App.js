6e634a159c300c21f03a86f36fd2a35beac9df57
jace <jaceemmanuel33@gmail.com>
2022-10-21 09:11:23 -1100

    responsive styling

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<MainPage/>}/>
+        <Route path="/" element={<Dashboard/>}/>
         <Route path="/dashboard" element={<Dashboard/>}/>
       </Routes>
     </Router>
